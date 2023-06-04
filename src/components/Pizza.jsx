1c3f5f65c3ce9f1756583c2d2454d52c7a84a33c
dhruv-upreti <dhruvdupreti@gmail.com>
2023-06-04 13:09:08 +0200

    Completed Home Screen Frontend

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,34 +1,78 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 export default function Pizza({ pizza }) {
+  const [quantity, setQuantity] = useState(1);
+  const [variant, setVariant] = useState("small");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
+    <div style={{ margin: "100px" }} className="shadow-lg p-3 mb-5 bg-white rounded">
       <h1>{pizza.name}</h1>
-      <img
-        src={pizza.image}
-        className="img-fluid"
-        style={{ height: "200px" }}
-      />
+      <img src={pizza.image} className="img-fluid" style={{ height: "300px", width: "300px" }} onClick={handleShow} />
 
       <div className="flex-container">
-        <div className="w-100">
-          <p>Varients</p>
-          <select>
-            {pizza.varients.map((varients) => {
-              return <option value={varients}>{varients}</option>;
-            })}
+        <div className="w-100 m-1">
+          <p>Variants</p>
+          <select
+            value={variant}
+            onChange={(e) => {
+              setVariant(e.target.value);
+            }}
+          >
+            {pizza.variants &&
+              pizza.variants.map((variant) => {
+                return <option value={variant}>{variant}</option>;
+              })}
           </select>
         </div>
 
         <div className="w-100">
           <p>Quantity</p>
         </div>
-        <select>
-          {[...Array(10).keys()].map((x, i) => {
+
+        <select
+          value={quantity}
+          onChange={(e) => {
+            setQuantity(e.target.value);
+          }}
+        >
+          {[...Array(9).keys()].map((x, i) => {
             return <option value={i + 1}> {i + 1}</option>;
           })}
         </select>
       </div>
+
+      <div className="flex-container">
+        <div>
+          <h1>Price: {pizza.prices[0][variant] * quantity} Rs/-</h1>
+        </div>
+
+        <div>
+          <button className="btn">
+            ADD TO CART
+          </button>
+        </div>
+      </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <ModalHeader closeButton>
+          <ModalTitle>{pizza.name}</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          <img src={pizza.image} className="img-fluid" style={{ height: "400px" }} />
+          <p>{pizza.description}</p>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn" onClick={handleClose}>
+            Close
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
--- src/pizzasdata.js
+++ src/pizzasdata.js
@@ -1,6 +1,6 @@
-const pizzas =[
+const pizzas = [
   {
-    name: "PEPPER BARBECUE CHICKEN",
+    name: "Chicken Nugget Burger",
     varients: ["small", "medium", "large"],
     prices: [
       {
@@ -10,11 +10,12 @@ const pizzas =[
       },
     ],
     category: "nonveg",
-    image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
-    description: "Pepper Barbecue Chicken I Cheese",
+    image:
+      "https://cdn.sanity.io/images/czqk28jt/prod_bk_de/0264a22eb855859331c2014bd4f9947ad6704c43-750x600.png?w=750&q=40&fit=max&auto=format",
+    description: "The Chicken Nugget Burger is a mouthwatering delight that combines crispy chicken nuggets with a flavorful burger. This delectable creation features tender chicken nuggets, perfectly seasoned and breaded, stacked high on a fluffy burger bun. With each bite, you'll savor the satisfying crunch of the chicken nuggets, complemented by the juicy and savory flavors of the burger. The combination of the succulent chicken nuggets and the classic burger components creates a delightful fusion of textures and tastes. This burger is a true treat for chicken lovers, offering a delicious and satisfying meal that will leave you craving for more.",
   },
   {
-    name: "Non Veg Supreme",
+    name: "Crispy Chicken",
     varients: ["small", "medium", "large"],
     prices: [
       {
@@ -24,12 +25,13 @@ const pizzas =[
       },
     ],
     category: "nonveg",
-    image: "https://www.dominos.co.in/files/items/Non-Veg_Supreme.jpg",
+    image:
+      "https://cdn.sanity.io/images/czqk28jt/prod_bk_de/70153c53c1ad4e196935e960753216499efddfe7-750x600.png?w=750&q=40&fit=max&auto=format",
     description:
       "Bite into supreme delight of Black Olives, Onions, Grilled Mushrooms, Pepper BBQ Chicken, Peri-Peri Chicken, Grilled Chicken Rashers",
   },
   {
-    name: "Golden Corn Pizza",
+    name: "BBQ Ranch Burge",
     varients: ["small", "medium", "large"],
     prices: [
       {
@@ -40,10 +42,11 @@ const pizzas =[
     category: "veg",
     description:
       "Corn over the base makes it look beautiful. It is served with tomato sauce and chili flakes are sprinkled over the topping according the taste. After mixing all the ingredients, it is baked by adding cheese and corn for added flavor to pizza. Corn adds health and sweet taste to the pizza.",
-    image: "https://www.crazymasalafood.com/wp-content/images/golden-1.jpg",
+    image:
+      "https://cdn.sanity.io/images/czqk28jt/prod_bk_de/1fbf75c69755ac6357eb76395591e24875e1ec5a-750x600.png?w=750&q=40&fit=max&auto=format",
   },
   {
-    name: "Jalapeno & Red Paprika Pizza",
+    name: "Spicy Jalapeno Burger",
     varients: ["small", "medium", "large"],
     prices: [
       {
@@ -53,12 +56,13 @@ const pizzas =[
       },
     ],
     category: "veg",
-    image: "https://www.crazymasalafood.com/wp-content/images/jalepeno.jpg",
+    image:
+      "https://cdn.sanity.io/images/czqk28jt/prod_bk_de/96d7d9fb99d8756a16beb8cbbb124dc82a8bba02-750x600.png?w=750&q=40&fit=max&auto=format",
     description:
       "This pizza is amazing and can become more delicious if we will add some more cheese in it. Ingredients are yeast, sugar, olive oil, salt, and all-purpose flour in a big bowl. After mixing all the ingredients, it is baked by adding Jalapeno and Paprika with corns over the cheese layer. The base is made crunchy to give it best taste. It can be made more tasty by sprinkling chili flakes and Oregano as per the taste.",
   },
   {
-    name: "Margerita",
+    name: "Hawaiian Teriyaki Burger",
     varients: ["small", "medium", "large"],
     prices: [
       {
@@ -69,12 +73,12 @@ const pizzas =[
     ],
     category: "veg",
     image:
-      "https://cdn.loveandlemons.com/wp-content/uploads/2019/09/margherita-pizza-500x500.jpg",
+      "https://cdn.sanity.io/images/czqk28jt/prod_bk_de/8a764b73db164280be66e120dc7addf14ffdbc65-750x600.png?w=750&q=40&fit=max&auto=format",
     description:
       "The pizza base is made by mixing yeast, sugar, olive oil, salt, and all-purpose flour in a big bowl. After mixing all the ingredients, it is baked by adding the cheese as topping over it. The base is perfectly prepared by adding single layer of cheese over it. It is mouth-watering pizza for cheese lovers.",
   },
   {
-    name: "Double Cheese Margherita Pizza",
+    name: "Plant based chilli burger",
     varients: ["small", "medium", "large"],
     prices: [
       {
@@ -83,9 +87,11 @@ const pizzas =[
       },
     ],
     category: "veg",
-    image: "https://www.crazymasalafood.com/wp-content/images/double-1.jpg",
+    image:
+      "https://static.kfc.de/images/items/lg/cheese-crunch.jpg?v=gOR9P4",
     description:
-      "This is a plain pizza which have cheese on it which is margherita and is delicious because of the loads of cheese. After mixing all the ingredients, it is baked by adding the cheese as topping over it. The base is perfectly prepared by adding double layer of cheese over it",
+      "This is a flavorful plant-based chilli burger that is packed with deliciousness. The patty is made from a blend of wholesome plant-based ingredients, carefully seasoned to create a savory and satisfying taste. Topped with fresh vegetables and a zesty vegan chilli sauce, this burger offers a burst of flavors. The bun is soft and perfectly toasted, providing a delightful texture. With its plant-based ingredients and bold flavors, this chilli burger is a mouthwatering option for those seeking a delicious and sustainable burger experience.",
   },
 ];
-export default pizzas
+export default pizzas;
+  
